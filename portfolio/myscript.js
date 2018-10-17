850a039b45861a2d35a77e7d8951c2a7e83fa016
David Zhao <dz6314@rit.edu>
2018-10-17 01:16:52 -0400

    Working on responsive for mobile, and tablets

--- portfolio/myscript.js
+++ portfolio/myscript.js
@@ -1,3 +1,19 @@
 /*This is the Javascript*/
 
-var vid;
+/*This function allows the works to toggle the subnav when on mobile mode*/
+function myFunction() {
+    var x = document.getElementById("subnav_mobile");
+    
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+
+        // document.getElementById("wrapper").style.marginTop = "60px";
+
+    } 
+    
+    else {
+        x.style.display = "none";
+        // document.getElementById("wrapper").style.marginTop = "0";
+    }
+}
