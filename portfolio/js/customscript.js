60b224a9a41077578f2dc7695fcf976370d45b99
David Zhao <dz6314@rit.edu>
2018-10-23 10:33:29 -0400

    adding side bar

--- portfolio/js/customscript.js
+++ portfolio/js/customscript.js
@@ -3,19 +3,36 @@
 /*This function allows the works to toggle the subnav when on mobile mode*/
 function myFunction() {
     var x = document.getElementById("subnav_mobile");
-    
+    var y = document.getElementById("grid-wrapper");
 
     if (x.style.display === "none") {
         x.style.display = "block";
 
-        // document.getElementById("wrapper").style.marginTop = "60px";
+       document.getElementById("grid-wrapper").style.paddingTop = "150px";
 
     } 
     
     else {
         x.style.display = "none";
-        // document.getElementById("wrapper").style.marginTop = "0";
+        document.getElementById("grid-wrapper").style.paddingTop = "0";
     }
+}
+// This code is created by w3schools.
+// Source: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+
+
+/*This function allows the works to toggle the side bar of the 
+Main Characters when on mobile mode*/
+function side_bar() {
+  var x = document.getElementById("sub_section");
+
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } 
+  
+  else {
+      x.style.display = "none";
+  }
 }
 // This code is created by w3schools.
 // Source: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
