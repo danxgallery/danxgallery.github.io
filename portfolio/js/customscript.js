8b049e971332e8bab12dc242765d04cbb9785b1b
David Zhao <dz6314@rit.edu>
2018-10-31 22:43:13 -0400

    updating javascript

--- portfolio/js/customscript.js
+++ portfolio/js/customscript.js
@@ -3,18 +3,18 @@
 /*This function allows the works to toggle the subnav when on mobile mode*/
 function myFunction() {
     var x = document.getElementById("subnav_mobile");
-    var y = document.getElementById("grid-wrapper");
 
     if (x.style.display === "none") {
         x.style.display = "block";
 
-       document.getElementById("grid-wrapper").style.paddingTop = "150px";
+       document.getElementById("moving").style.paddingTop = "150px";
+       
 
     } 
     
     else {
         x.style.display = "none";
-        document.getElementById("grid-wrapper").style.paddingTop = "0";
+        document.getElementById("moving").style.paddingTop = "0";
     }
 }
 // This code is created by w3schools.
@@ -25,6 +25,7 @@ function myFunction() {
 Main Characters when on mobile mode*/
 function side_bar() {
   var x = document.getElementById("sub_section");
+
 
   if (x.style.display === "none") {
       x.style.display = "block";
@@ -37,7 +38,84 @@ function side_bar() {
 // This code is created by w3schools.
 // Source: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 
+function section_button() {
+    var x = document.getElementById("section0");
+  
+  
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    
+    else {
+        x.style.display = "none";
+    }
+  }
+  // This code is created by w3schools.
+  // Source: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 
+  function section_button1() {
+    var x = document.getElementById("section1");
+  
+  
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    
+    else {
+        x.style.display = "none";
+    }
+  }
+  // This code is created by w3schools.
+  // Source: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+
+  function section_button2() {
+    var x = document.getElementById("section2");
+  
+  
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    
+    else {
+        x.style.display = "none";
+    }
+  }
+  // This code is created by w3schools.
+  // Source: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+
+  function section_button3() {
+    var x = document.getElementById("section3");
+  
+  
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    
+    else {
+        x.style.display = "none";
+    }
+  }
+  // This code is created by w3schools.
+  // Source: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+
+//   function back() {
+//     var x = document.getElementById("side_sub");
+  
+  
+//     if (x.style.left === "-330px") {
+//         x.style.left = "0";
+//         console.log("working");
+ 
+//     } 
+    
+//     else {
+//         x.style.left = "-330px";
+
+//         console.log("not working");
+//     }
+//   }
+  // This code is created by w3schools.
+  // Source: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 /***********************************************************
 ------------------- JQuery --------------------------------
 *****************************************************************/
